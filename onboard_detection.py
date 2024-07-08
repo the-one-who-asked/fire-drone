cabcf2c6814f5aef5dd3e45d9bd83f87cd69fe7d
the-one-who-asked <ansh.arora0512@gmail.com>
2024-07-08 10:55:26 +0200

    optimisations

--- onboard_detection.py
+++ onboard_detection.py
@@ -1,9 +1,7 @@
 """
-Detects fires in IR images using a rule-based system which exhausts few of the drone's resources and is optimised using
-the onboard GPU.
-
-IR images are cleaned using temporal filtering and the fire detection method minimises false negatives so that frames
-which are believed to show evidence of fire can be sent to a server with a more powerful detection algorithm.
+Detects fires in images using a rule-based system which will be optimised using the onboard GPU. Frames are cleaned
+using temporal filtering. The fire detection method minimises false negatives so that frames which are believed to show
+evidence of fire can be sent to a server with a more powerful detection algorithm.
 """
 
 "====================================================== Imports ======================================================="
@@ -17,29 +15,33 @@ SIZE = (1080, 1920)
 FPS = 30
 SPF = 1/FPS
 IMG_AREA = (9, 16)
-SAIL_SEARCH = False
-FIRE_THRESHOLD = 2
+SAIL_SEARCH = False  # Indicates whether the drone is rotating freely or is moving in straight lines
+Y_THRESHOLD = 1.5
+CR_MIN, CR_MAX = 135, 180
+CB_MIN, CB_MAX = 85, 135
 
 # Variables
-delta_t = 0
 speed = 1
 theta = 0
 d_theta = 0
 img_speed = (SIZE[1] * speed) // (IMG_AREA[1] * FPS)
 underlap = SIZE[1] // img_speed
-frames = [np.zeros(SIZE, dtype=np.uint8) for _ in range(min(underlap, FPS))]  # Queue
-underlaps = [SIZE[0]] * min(underlap, FPS)                                    # Queue
+frames = np.zeros((min(underlap, FPS),) + SIZE, dtype=np.int8)  # Queue
 
 "====================================================== Modules ======================================================="
 def get_frame() -> np.ndarray:
     """Captures a new frame from the NoIR camera"""
 
+    # capture frame in YUV
+    # convert from YUV to YCbCr
     return np.zeros(SIZE)
 
 
 def update_speed():
+    """Modifies velocity and angular velocity based on sensor readings"""
+
     global speed, theta, d_theta, img_speed
-    theta += d_theta * delta_t
+    theta += d_theta * SPF
     theta %= 360
 
 
@@ -51,19 +53,17 @@ def rotate_img(frame: np.ndarray, angular_velocity: float) -> np.ndarray:
     return cv2.warpAffine(frame, rot_mat, (SIZE[1], SIZE[0]), flags=cv2.INTER_LINEAR)
 
 
-def clean_frame(frame: np.ndarray):
+def clean_frame(frame: np.ndarray) -> np.ndarray:
     """Removes noise from new frames through the use of temporal filtering"""
 
     # Initialises variables depending on whether the drone is rotating
-    global underlaps
     prev_frame = frames[-1]
-    silhouette = np.ones(SIZE)
     if SAIL_SEARCH:
         i = d_theta
-        difference = lambda: np.sum(abs(rotate_img(prev_frame, i) - frame) * rotate_img(silhouette, i))
+        difference = lambda: np.sum(((mask := rotate_img(prev_frame, i)) - frame[mask]) ** 2)
     else:
         i = img_speed
-        difference = lambda: np.sum(abs(prev_frame[i:] - frame[:-i]))
+        difference = lambda: np.sum((prev_frame[i:] - frame[:-i]) ** 2)
 
     # Maximises the overlap between consecutive frames
     current = difference()
@@ -91,43 +91,38 @@ def clean_frame(frame: np.ndarray):
             i += step
 
     # Updates the queues of underlaps and frames to reflect the overlap between each frame and the current one
-    frames.pop(0)
-    frames.append(frame)
-    underlaps.pop(0)
-    underlaps.append(0)
-    for idx in range(len(underlaps)):
-        underlaps[idx] += i
-
-    # Gets the median pixel at regions of overlap, removing any noise and ensuring that fires detected are growing
+    frames[:-1] = frames[1:]
     if SAIL_SEARCH:
-        for i, (prev, current) in zip(underlaps, underlaps[1:] + [0]):
-            mask = np.where(rotate_img(silhouette, current) != 0 and rotate_img(silhouette, prev) == 0)
-            frame[mask] = frames[SIZE - i // -2][mask]
+        for j, frame in enumerate(frames):
+            frames[j] = rotate_img(frame, -i)
     else:
-        for i, (prev, current) in zip(underlaps, underlaps[1:] + [0]):
-            frame[prev:current] = frames[SIZE - i // -2][prev:current]
+        frames[:, i:] = 0
+    frames[-1] = frame
+
+    # Gets the median pixel at regions of overlap, removing any noise and ensuring that fires detected are persistent
+    return np.median(frames[:, frames[0].astype(bool)], axis=0)
 
 
 def fire_pixels(frame: np.ndarray) -> np.ndarray:
-    """Finds pixels with relatively hight temperatures"""
+    """Finds pixels with relatively high intensities and red components"""
 
-    st_dev = np.std(frame, ddof=1)
-    return frame > FIRE_THRESHOLD * np.std(frame, ddof=1)
+    y, cr, cb = cv2.split(frame)
+    return (y > np.mean(y) + Y_THRESHOLD * np.std(y, ddof=1)) and (CR_MIN <= cr <= CR_MAX) and (CB_MIN <= cb <= CB_MAX)
 
 
 def transmit(frame: np.ndarray, mask: np.ndarray):
     """Sends a frame to the server using a radio transmitter"""
 
-    pass
+    print(frame)
+    print(mask)
 
 "======================================================== Main ========================================================"
 def main():
-    global delta_t
     while True:
         delta_t = time()
         frame = get_frame()
-        clean_frame(frame)
-        mask = fire_pixels(frame)
+        filtered = clean_frame(frame)
+        mask = fire_pixels(filtered)
         if mask.any():
             transmit(frame, mask)
         delta_t -= time()
